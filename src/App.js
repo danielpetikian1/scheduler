5fad438837f23b733100c2e52642cfb4262d1ece
digitalsignalprocess <58404228+digitalsignalprocess@users.noreply.github.com>
2021-10-08 17:24:51 -0500

    halfway through selecting by term

--- src/App.js
+++ src/App.js
@@ -15,22 +15,68 @@ const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
 
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-      <div className="card-text">{ course.title }</div>
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const style = {
+    backgroundColor: isSelected ? 'lightgreen' : 'white'
+  };
+  return (
+    <div className="card m-1 p-2" 
+      style={style}
+      onClick={() => setSelected(toggle(course, selected))}>
+      <div className="card-body">
+        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+        <div className="card-text">{ course.title }</div>
+        <div className="card-text">{ course.meets }</div>
+      </div>
     </div>
-  </div>
+  );
+};
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+      { 
+        termCourses.map(course =>
+          <Course key={ course.id } course={ course }
+            selected={selected} setSelected={ setSelected } 
+          />) 
+      }
+      </div>
+    </>
+  );
+};
+
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
 
-const CourseList = ({ courses }) => (
-  <div className="course-list">
-  { Object.values(courses).map(course => <Course key={course.id} course={ course } />) }
-  </div>
+
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+      onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
 );
 
-
+const TermSelector = ({term, setTerm}) => (
+  <div className="btn-group">
+  { 
+    Object.values(terms).map(value => (
+      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+    ))
+  }
+  </div>
+);
 
 
 
