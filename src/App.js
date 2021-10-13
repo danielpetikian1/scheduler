44076b2ce6a065d71109913a34c04073061a15a6
digitalsignalprocess <58404228+digitalsignalprocess@users.noreply.github.com>
2021-10-12 23:43:04 -0500

    read from a database

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import { hasConflict, terms, getCourseTerm, getCourseNumber } from './utilities/times.js';
 import CourseList from './components/CourseList';
+import { useData } from './utilities/firebase.js';
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
@@ -35,21 +36,11 @@ const addScheduleTimes = schedule => ({
 });
 
 const App = () => {
-  const [schedule, setSchedule] = useState();
-  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  const [schedule, loading, error] = useData('/', addScheduleTimes); 
+  
+  if (error) return <h1>{error}</h1>;
+  if (loading) return <h1>Loading the schedule...</h1>
 
-  useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(addScheduleTimes(json));
-    }
-    fetchSchedule();
-  }, [])
-
-
-  if (!schedule) return <h1>Loading schedule...</h1>;
   return (
     <div className="container">
       <Banner title={ schedule.title } />
@@ -57,4 +48,5 @@ const App = () => {
     </div>
   );
 };
+
 export default App;
---
+++ src/utilities/firebase.js
@@ -0,0 +1,49 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { useState, useEffect} from 'react';
+import { getDatabase, onValue, ref, set } from 'firebase/database';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDmo0ZaREru_RAEN-YYMkTU5osxmx32ChY",
+  authDomain: "scheduler-63238.firebaseapp.com",
+  databaseURL: "https://scheduler-63238-default-rtdb.firebaseio.com",
+  projectId: "scheduler-63238",
+  storageBucket: "scheduler-63238.appspot.com",
+  messagingSenderId: "373081194202",
+  appId: "1:373081194202:web:c2b06ec0eb8f3849072fe3",
+  measurementId: "G-XFYEPP3NTZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export const useData = (path, transform) => {
+    const [data, setData] = useState();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState();
+  
+    useEffect(() => {
+      const dbRef = ref(database, path);
+      const devMode = !process.env.NODE_ENV || process.env.NODE_ENV === 'development';
+      if (devMode) { console.log(`loading ${path}`); }
+      return onValue(dbRef, (snapshot) => {
+        const val = snapshot.val();
+        if (devMode) { console.log(val); }
+        setData(transform ? transform(val) : val);
+        setLoading(false);
+        setError(null);
+      }, (error) => {
+        setData(null);
+        setLoading(false);
+        setError(error);
+      });
+    }, [path, transform]);
+  
+    return [data, loading, error];
+  };
